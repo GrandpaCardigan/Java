09d340b6078b1c501ae15afba2230d6b9defe2c4
Nicholas Kier <nicholas.k.kier@gmail.com>
2019-02-13 18:16:19 -0500

    Add files via upload

--- treehouse/src/Example.java
+++ treehouse/src/Example.java
@@ -1,8 +1,32 @@
 public class Example {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("We are making a pez dispenser.");
-        PezDispenser dispenser = new PezDispenser("Donatello");
+        System.out.printf("Fun fact, there are %d PEZ allowed in every dispenser.",
+                PezDispenser.MAX_PEZ);
+        PezDispenser dispenser = new PezDispenser("Yoda");
         System.out.printf("The dispenser is %s %n", dispenser.getCharacterName()
         );
+        if (dispenser.isEmpty()){
+            System.out.println("Dispenser is empty.");
+        }
+        System.out.println("Filling the dispenser with delicious Pez...");
+        dispenser.fill();
+        if (!dispenser.isEmpty()){
+            System.out.println("Dispenser is full.");
+        }
+        while(dispenser.dispense()){
+            System.out.println("Chomp!");
+        }
+        if(dispenser.isEmpty()){
+            System.out.println("Ate all the PEZ!");
+        }
+        try{
+            dispenser.fill(400);
+            System.out.println("This will never happen.");
+        } catch(IllegalArgumentException iae){
+            System.out.println("Whoa there!");
+            System.out.printf("The error was %s",
+                    iae.getMessage());
+        }
     }
 }
--- treehouse/src/PezDispenser.java
+++ treehouse/src/PezDispenser.java
@@ -1,11 +1,39 @@
 public class PezDispenser {
-    private String characterName;
+    public static final int MAX_PEZ = 12;
+    final private String characterName; //final says that characterName can only be initiliazed once
+    private int pezCount;
 
-    public PezDispenser(String characterName){
+    public PezDispenser(String characterName) {
         this.characterName = characterName;
-
+        pezCount = 0;
     }
-    public String getCharacterName(){
+
+    public void fill() {
+        pezCount = MAX_PEZ;
+    }
+
+    public void fill(int pezAmount) {
+        int newAmount = pezCount + pezAmount;
+        if(newAmount > MAX_PEZ){
+            throw new IllegalArgumentException("Too many PEZ!!!!");
+        }
+        pezCount = newAmount;
+    }
+
+    public boolean dispense() {
+        boolean wasDispensed = false;
+        if (!isEmpty()) {
+            pezCount--;
+            wasDispensed = true;
+        }
+        return wasDispensed;
+    }
+
+    public boolean isEmpty() {
+        return pezCount == 0;
+    }
+
+    public String getCharacterName() {
         return characterName;
     }
 }
